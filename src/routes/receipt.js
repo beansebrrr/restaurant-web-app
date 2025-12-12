1d956c4fbf6f96ae0f44214482bf29fb3d6c485f
beanse <173890355+beansebrrr@users.noreply.github.com>
2025-12-13 01:20:41 +0400

    Refresh basket after checkout

--- src/routes/receipt.js
+++ src/routes/receipt.js
@@ -6,6 +6,7 @@ router.route("/")
 .get(async function (req, res) {
   let basketContents = await BasketItem.find({}).populate("item").exec();
   if (basketContents.length > 0) {
+    await BasketItem.deleteMany({}).exec();
     res.render("receipt/index", { foods: basketContents })
   } else {
     res.redirect("/basket")
