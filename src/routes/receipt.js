7155ffe9ad40cc58ea140a34d7f3d48dc5c1d833
beanse <173890355+beansebrrr@users.noreply.github.com>
2025-12-13 01:15:39 +0400

    Receipt page styled and polished

--- src/routes/receipt.js
+++ src/routes/receipt.js
@@ -5,12 +5,12 @@ import BasketItem from "../models/basket.model.js";
 router.route("/")
 .get(async function (req, res) {
   let basketContents = await BasketItem.find({}).populate("item").exec();
-  if (basketContents) {
+  if (basketContents.length > 0) {
     res.render("receipt/index", { foods: basketContents })
+  } else {
+    res.redirect("/basket")
   }
 })
-
-
 
 
 export default router
